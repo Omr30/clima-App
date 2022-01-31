f9f41b5f69ba443dafa6e2ef659b7498a06cda5a
ozkr <oscarmenacho23@gmail.com>
2022-01-30 20:36:07 -0500

    Modelo para controlar la app fix

---
+++ models/busquedas.js
@@ -0,0 +1,19 @@
+
+
+class Busquedas {
+    historial = ['Tegucigalpa', 'Madrid', 'Lima'];
+
+    construnctor() {
+        //TODO: leer DB si existe
+    }
+
+    async ciudad( lugar='' ) {
+        //peticion http
+        console.log(lugar);
+
+        return [];
+    }
+
+}
+
+module.exports = Busquedas;
