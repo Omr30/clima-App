fdabde2531763d448815a2b8a45af436c4beaefb
ozk <oscarmenacho23@gmail.com>
2022-02-18 23:26:32 -0600

    Crear instancias de Axios

--- models/busquedas.js
+++ models/busquedas.js
@@ -7,12 +7,24 @@ class Busquedas {
         //TODO: leer DB si existe
     }
 
+    get paramsMapbox() {
+        return {
+            'language': 'es',
+            'access_token': 'pk.eyJ1Ijoib3prMzAiLCJhIjoiY2t6ZG1yYWZrMno0dTMxdHZlZmFja3diMSJ9.DgU_JPeVLyo25mzpTzAK_A'
+        }
+    }
+
     async ciudad( lugar='' ) {
 
         try {
-            //peticion http
-            // console.log('ciudad', lugar);
-            const resp = await axios.get('https://api.mapbox.com/geocoding/v5/mapbox.places/Madrid.json?language=es&access_token=pk.eyJ1Ijoib3prMzAiLCJhIjoiY2t6ZG1yYWZrMno0dTMxdHZlZmFja3diMSJ9.DgU_JPeVLyo25mzpTzAK_A');
+            //Peticion http
+            
+            const instance = axios.create({
+                baseURL: `https://api.mapbox.com/geocoding/v5/mapbox.places/${ lugar }.json`,
+                params: this.paramsMapbox
+            });
+
+            const resp = await instance.get();
             console.log(resp.data);
     
             return []; // retornar lugares
