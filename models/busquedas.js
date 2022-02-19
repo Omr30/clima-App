3914722ef3cd15047b5d77adcb15ef8bb942bf12
ozk <oscarmenacho23@gmail.com>
2022-02-18 23:16:21 -0600

    Mapbox Search API y Token de acceso

--- models/busquedas.js
+++ models/busquedas.js
@@ -8,12 +8,18 @@ class Busquedas {
     }
 
     async ciudad( lugar='' ) {
-        //peticion http
-        // console.log('ciudad', lugar);
-        const resp = await axios.get('https://reqres.in/api/users?page=2');
-        console.log(resp.data);
 
-        return []; // retornar lugares
+        try {
+            //peticion http
+            // console.log('ciudad', lugar);
+            const resp = await axios.get('https://api.mapbox.com/geocoding/v5/mapbox.places/Madrid.json?language=es&access_token=pk.eyJ1Ijoib3prMzAiLCJhIjoiY2t6ZG1yYWZrMno0dTMxdHZlZmFja3diMSJ9.DgU_JPeVLyo25mzpTzAK_A');
+            console.log(resp.data);
+    
+            return []; // retornar lugares
+
+        } catch (error) {
+            return[];
+        }
     }
 
 }
